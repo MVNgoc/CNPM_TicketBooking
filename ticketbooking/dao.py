7a43a1d27c4f53f78ebe70c997e510926ea30d5c
ngocm <ngocminhle251@gmail.com>
2024-04-28 22:18:21 +0700

    update UI1

--- ticketbooking/dao.py
+++ ticketbooking/dao.py
@@ -9,3 +9,7 @@ def load_categories():
 def load_book_ticket_step():
     with open('%s/data/bookticketstep.json' % app.root_path, encoding='utf-8') as f:
         return json.load(f)
+
+def load_list_of_ticket_step():
+    with open('%s/data/listofticketstep.json' % app.root_path, encoding='utf-8') as f:
+        return json.load(f)
