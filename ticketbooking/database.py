d9f1aa85f2ebf9208458ac71ed52a6c64d7629bb
ngocm <ngocminhle251@gmail.com>
2024-05-07 16:13:55 +0700

    update sample

--- ticketbooking/database.py
+++ ticketbooking/database.py
@@ -9,12 +9,28 @@ def create_database():
 
 
 def add_sample_data():
-    airport1 = Airport(airportID='TSN', airportName='Tan Son Nhat International Airport')
-    airport2 = Airport(airportID='PQC', airportName='Phu Quoc International Airport')
+    airport1 = Airport(airportID='TSN', airportName='Tan Son Nhat International Airport', locationCity='Hồ Chí Minh')
+    airport2 = Airport(airportID='PQC', airportName='Phu Quoc International Airport', locationCity='Phú Quốc')
+    airport3 = Airport(airportID='HAN', airportName='Noi Bai International Airport', locationCity='Hà Nội')
+    airport4 = Airport(airportID='DAD', airportName='Da Nang International Airport', locationCity='Đà Nẵng')
+    airport5 = Airport(airportID='VDO', airportName='Van Don International Airport', locationCity='Quảng Ninh')
+    airport6 = Airport(airportID='HUI', airportName='Phu Bai International Airport', locationCity='Huế')
+
 
     route1 = Route(routeID='R001', routeName='HCMC-PQC', departureAirportID='TSN', arrivalAirportID='PQC')
+    route2 = Route(routeID='R002', routeName='HCMC-HNC', departureAirportID='TSN', arrivalAirportID='HAN')
+    route3 = Route(routeID='R003', routeName='HCMC-DNC', departureAirportID='TSN', arrivalAirportID='DAD')
+    route4 = Route(routeID='R004', routeName='PQC-HCMC', departureAirportID='PQC', arrivalAirportID='TSN')
+    route5 = Route(routeID='R005', routeName='HCMC-HC', departureAirportID='TSN', arrivalAirportID='HUI')
+    route6 = Route(routeID='R006', routeName='HC-HCMC', departureAirportID='HUI', arrivalAirportID='TSN')
 
     flight1 = Flight(flightID='F001', routeID='R001', departureTime=datetime.strptime('2024-4-20 08:00:00','%Y-%m-%d %H:%M:%S'), arrivalTime=datetime.strptime('2024-4-20 11:00:00','%Y-%m-%d %H:%M:%S'), numOf1stClassSeat=100, numOf2ndClassSeat=200, flightStatus='Scheduled', availableSeats=300)
+    flight2 = Flight(flightID='F002', routeID='R001', departureTime=datetime.strptime('2024-4-20 08:00:00','%Y-%m-%d %H:%M:%S'), arrivalTime=datetime.strptime('2024-4-20 11:00:00','%Y-%m-%d %H:%M:%S'), numOf1stClassSeat=100, numOf2ndClassSeat=200, flightStatus='Scheduled', availableSeats=300)
+    flight3 = Flight(flightID='F003', routeID='R001', departureTime=datetime.strptime('2024-4-21 08:00:00','%Y-%m-%d %H:%M:%S'), arrivalTime=datetime.strptime('2024-4-21 11:00:00','%Y-%m-%d %H:%M:%S'), numOf1stClassSeat=100, numOf2ndClassSeat=200, flightStatus='Scheduled', availableSeats=300)
+    flight4 = Flight(flightID='F004', routeID='R002', departureTime=datetime.strptime('2024-4-20 08:00:00','%Y-%m-%d %H:%M:%S'), arrivalTime=datetime.strptime('2024-4-20 11:00:00','%Y-%m-%d %H:%M:%S'), numOf1stClassSeat=100, numOf2ndClassSeat=200, flightStatus='Scheduled', availableSeats=300)
+    flight5 = Flight(flightID='F005', routeID='R002', departureTime=datetime.strptime('2024-4-21 08:00:00','%Y-%m-%d %H:%M:%S'), arrivalTime=datetime.strptime('2024-4-21 11:00:00','%Y-%m-%d %H:%M:%S'), numOf1stClassSeat=100, numOf2ndClassSeat=200, flightStatus='Scheduled', availableSeats=300)
+    flight6 = Flight(flightID='F006', routeID='R002', departureTime=datetime.strptime('2024-4-21 08:00:00','%Y-%m-%d %H:%M:%S'), arrivalTime=datetime.strptime('2024-4-21 11:00:00','%Y-%m-%d %H:%M:%S'), numOf1stClassSeat=100, numOf2ndClassSeat=200, flightStatus='Scheduled', availableSeats=300)
+
 
     seat_class1 = SeatClass(classID='SC001', className='First Class', maxCheckedWeight=32, maxCarryOnWeight=10)
     seat_class2 = SeatClass(classID='SC002', className='Economy Class', maxCheckedWeight=20, maxCarryOnWeight=7)
@@ -23,8 +39,16 @@ def add_sample_data():
     price2 = Price(priceID='P002', flightID='F001', classID='SC002', price=50.0)
 
     customer1 = Customer(customerID='C001', customerName='John Doe', gender='Male', birthDate=datetime.strptime('1997-4-6 00:00:00','%Y-%m-%d %H:%M:%S'), idNumber=123456789, phoneNumber=987654321)
+    customer2 = Customer(customerID='C002', customerName='Abc', gender='Female', birthDate=datetime.strptime('1997-4-6 00:00:00','%Y-%m-%d %H:%M:%S'), idNumber=123456789, phoneNumber=987654321)
+
 
     invoice1 = Invoice(invoiceID=1, customerID='C001', paymentAmount=100.0, paymentStatus='Paid', paymentMethod='Cash', paymentTime=datetime.strptime('2024-4-18 16:30:00','%Y-%m-%d %H:%M:%S'))
+    invoice2 = Invoice(invoiceID=2, customerID='C001', paymentAmount=100.0, paymentStatus='Paid', paymentMethod='Cash', paymentTime=datetime.strptime('2024-4-19 16:30:00','%Y-%m-%d %H:%M:%S'))
+    invoice3 = Invoice(invoiceID=3, customerID='C001', paymentAmount=100.0, paymentStatus='Paid', paymentMethod='Cash', paymentTime=datetime.strptime('2024-4-20 16:30:00','%Y-%m-%d %H:%M:%S'))
+    invoice4 = Invoice(invoiceID=4, customerID='C002', paymentAmount=100.0, paymentStatus='Paid', paymentMethod='Cash', paymentTime=datetime.strptime('2024-4-20 16:30:00','%Y-%m-%d %H:%M:%S'))
+    invoice5 = Invoice(invoiceID=5, customerID='C002', paymentAmount=100.0, paymentStatus='Paid', paymentMethod='Cash', paymentTime=datetime.strptime('2024-4-22 16:30:00','%Y-%m-%d %H:%M:%S'))
+    invoice6 = Invoice(invoiceID=6, customerID='C002', paymentAmount=100.0, paymentStatus='Paid', paymentMethod='Cash', paymentTime=datetime.strptime('2024-4-23 16:30:00','%Y-%m-%d %H:%M:%S'))
+
 
     ticket1 = Ticket(invoiceID=1, customerID='C001', accountID='1', flightID='F001', classID='SC001', priceID='P001', bookingTime=datetime.strptime('2024-4-18 16:30:00','%Y-%m-%d %H:%M:%S'), paid=True)
 
