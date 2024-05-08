8c964a7864670b27638009c33446bc2b2d93d569
MaiNgoc <ngoc.mai@tititada.com>
2024-05-08 09:32:28 +0700

    add secret_key

--- ticketbooking/__init__.py
+++ ticketbooking/__init__.py
@@ -8,6 +8,8 @@ app.config[
     'SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:%s@localhost/flight_ticket_booking_system?charset=utf8mb4' % quote(
     'TDtu288392200114@')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.secret_key = '!@@!#@!#!@sdkjfasdadhadkhdjahdsaldha!@#!@#@!#@!'
 db = SQLAlchemy(app=app)
+
 
 login = LoginManager(app)
